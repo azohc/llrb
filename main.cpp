7562d460bba947298f3baabf5e29e78b314da2bc
azohc <juanchozass@gmail.com>
2018-12-28 12:06:28 +0100

    SAas

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@
 using std::cin;
 using std::cout;
 using std::endl;
-using std::fstream;
+using std::ifstream;
 using std::istream;
 
 
@@ -30,6 +30,9 @@ static utime_t get_time ()
     gettimeofday(&now, NULL);
     return  now.tv_usec + (utime_t)now.tv_sec * 1000000;
 }
+
+
+
 
 // Test LLRB Tree with random pairs of keys and values
 // executing max_elems insertions, searches and deletions num_tests times
@@ -116,11 +119,44 @@ int stresstest(int num_tests, int max_elems, bool verbose)
     return 0;
 }
 
+void opt_st()
+{
+    int n_tests, m_elems;
+    char c;
+    cout << endl << "maximum number of elements to test with: ";
+    cin >> m_elems;
+    while(m_elems < 1)
+    {
+        cout << "enter a positive number: ";
+        cin >> m_elems;
+    }
+    
+    cout << "number of tests to execute: ";
+    cin >> n_tests;
+    while(n_tests < 1)
+    {
+        cout << "enter a positive number: ";
+        cin >> m_elems;
+    }
+
+    cout << "print output messages? (y/n): ";
+    cin >> c;
+    while(tolower(c) != 'y' && tolower(c) != 'n')
+    {
+        cout << "enter y for yes or n for no: ";
+        cin >> c;
+    }  
+
+
+    stresstest(n_tests, m_elems, true);
+}
+
+
 template <class K, class V>
 LLRBTree<K,V> readtree(const char* path)
 {
-    LLRBTree<K,V> t;
-    fstream f;
+    LLRBTree<int,char> t;
+    ifstream f;
     f.open(path);
 
     if(f.is_open())
@@ -132,16 +168,104 @@ LLRBTree<K,V> readtree(const char* path)
         return t;
     }
 
+    K n;
+    V c;
+    while (!f.eof())
+    {
+        f >> n >> c;
+        t.insert(n, c);
+    }
+    
+    f.close();
     return t;
 }
 
+template <class K, class V>
+void benchmark_insert(LLRBTree<K,V> &t, const std::vector<K> &keys, const std::vector<V> &vals)
+{
+    while(!keys.empty() && !vals.empty())
+    {
+        t.insert(keys.back(), vals.back());
+        keys.pop_back();
+        vals.pop_back();
+    }
+}
+
+int getchoice(){
+    int choice = -1;
+    cout << "left leaning red black tree test menu" << endl 
+        << "\t2. \t->\t stress test" << endl
+        << "\t3. \t->\t read tree from file" << endl
+        << "\t4. \t->\t test insertion" << endl
+        << "\t5. \t->\t test search" << endl
+        << "\t6. \t->\t test deletion" << endl
+        << "\t1. \t->\t print help" << endl
+        << "\t0. \t->\t exit" << endl;
+
+    cout << "enter your option: ";
+    cin >> choice;
+    while(choice < 0 || choice > 6)
+    {
+        cout << "enter your option (0 to 6): ";
+        cin >> choice;
+    }
+    return choice;
+}
+
+
 int main()
 {
-    // stresstest(10, 500, true);    
-    LLRBTree <int,char*> t_hh = readtree<int,char*>("test_trees/hiphop.txt");
+    int choice = getchoice();
+    while(choice)
+    {
+        switch(choice)
+        {
+            case 1:
 
+                break;
+            case 2:
+            
+                opt_st();
+                break;
+            case 3:
+            
+                break;
+            case 4:
+            
+                break;
+            case 5:
+            
+                break;
+            case 6: 
+
+                break;
+        }
+        choice = getchoice();
+    }
 
     return 0;
+    
+    /*-----------------
+    stresstest(x, y, v)
+        x loops of y insertions, searches and deletions in a tree.
+        v (bool) verbose mode, enables/disables the printing of output messages 
+    --------------*/
+        
+
+
+    //LLRBTree <int,char> t_hh = readtree<int,char>("test_trees/test1.txt");
+
+    /*---------------
+    
+    --------------*/
+
+
+    //fun to measure insertion time
+
+
+    //fun to measure search time
+    //fun to measure deletion time
+
 }
 
 
