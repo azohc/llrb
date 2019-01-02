338b9e3be4a974b4691f26773950cea8dd0ab827
azohc <juanchozass@gmail.com>
2019-01-02 20:50:50 +0100

    pngs

--- gnu/plot.sh
+++ gnu/plot.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/gnuplot
 
-set title "LLRB - Inserción"
-set xlabel "Elementos insertados"
+set title "LLRB - Buscar"
+set xlabel "Elementos buscados"
 set ylabel "Tiempo (ms)"
 #set ytics "100"
-plot "data"  with lines
+plot "data" title "busqueda" with lines
 pause -1 "Hit any key to continue"
