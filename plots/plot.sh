be0d7c052d99292632be074a4da1e2de41da393a
azohc <juanchozass@gmail.com>
2019-01-06 20:46:25 +0100

    fin..

--- plots/plot.sh
+++ plots/plot.sh
@@ -1,21 +1,21 @@
 #! /usr/bin/gnuplot
 
-# set title "tiempo medio de la ejecucion de una operacion"
-# set ylabel "tiempo medio (ms)"
-# set xlabel "elementos (miles)"
-# set ytics 0.0005
-# set xtics 20
-# plot "opinsert.txt" u 1:2 with lines title "insertar"
-# replot "opsearch.txt" u 1:2 with lines title "buscar"
-# replot "opdelete.txt" u 1:2 with lines  title "borrar"
-
-set title "tiempo medio de la ejecucion de n operaciones"
-set ylabel "tiempo medio (ms)"
+set title "media del tiempo transcurrido durante la ejecucion de una operacion"
+set ylabel "tiempo promedio (ms)"
 set xlabel "elementos (miles)"
 set ytics 0.0005
-set xtics 20
-plot "testinsert.txt" with lines  title "insertar"
-replot "testsearch.txt" with lines  title "buscar"
-replot "testdelete.txt" with lines  title "borrar"
+set xtics 250
+plot "opinsert.txt" u 1:2 with lines title "insertar"
+replot "opsearch.txt" u 1:2 with lines title "buscar"
+replot "opdelete.txt" u 1:2 with lines  title "borrar"
+
+# set title "media del tiempo transcurrido durante la ejecucion de 'e' operaciones"
+# set ylabel "tiempo promedio (ms)"
+# set xlabel "elementos (miles)"
+# set xtics 500
+# set ytics 25
+# plot "testinsert.txt" with lines  title "insertar"
+# replot "testsearch.txt" with lines  title "buscar"
+# replot "testdelete.txt" with lines  title "borrar"
 
 pause -1
